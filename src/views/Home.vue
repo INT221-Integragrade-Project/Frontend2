83fb42b6170235a9c024e33142c32ea9863b652a
WinnieZero <57484936+WinnieZero@users.noreply.github.com>
2021-04-28 10:05:55 +0700

    [first commit] home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,18 +23,6 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-// export default {
-//   name: 'Home',
-//   components: {
-//     HelloWorld
-//   }
-// }
-</script>
-
 <style>
 
 .home {
@@ -67,3 +55,16 @@
 }
 
 </style>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
